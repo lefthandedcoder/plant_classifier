76eeaf9716b8416112e9761768eda4d95aeaedb4
Christian Dye <christiancdye@gmail.com>
2022-01-08 16:16:50 -0600

    commit

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import os
-import uuid
-import flask
-import urllib
-from PIL import Image
 from tensorflow.keras.models import load_model
 from flask import Flask, render_template, request
 from tensorflow.keras.preprocessing.image import load_img , img_to_array
