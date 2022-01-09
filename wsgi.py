cd9a63279bbcad1bfe62ad70705904246b48d32c
Christian Dye <christiancdye@gmail.com>
2022-01-08 18:46:25 -0600

    commit

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,11 @@
-from app import app
+# import flask app but need to call it "application" for WSGI to work
+from app import app as application  # noqa
+
 if __name__ == "__main__":
-    app.run()
+    application.run()
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
