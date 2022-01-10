4b168aea464f23eb08a8ba3d23ff0bf6acdd9135
Christian Dye <christiancdye@gmail.com>
2022-01-09 22:10:59 -0600

    app changes

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import os
+from PIL import Image, ImageFile
+import io
+from io import BytesIO
 from tensorflow.keras.models import load_model
-from flask import Flask, render_template, request
+import base64
+from flask import Flask, render_template, request, url_for
 from werkzeug.utils import secure_filename
-from tensorflow.keras.preprocessing.image import load_img , img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.backend import expand_dims
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ALLOWED_EXT = set(['jpg', 'jpeg', 'png'])
-UPLOAD_FOLDER = 'tmp'
-
-app = Flask(__name__)
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-model = load_model(os.path.join(BASE_DIR, 'model.hdf5'))
+ALLOWED_EXT = set(['jpg', 'jpeg', 'png', 'gif'])
+IMAGE_HEIGHT = 224
+IMAGE_WIDTH = 224
+IMAGE_CHANNELS = 3
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXT
 
 
 classes = ['alfalfa',
@@ -56,29 +54,8 @@ classes = ['alfalfa',
            'pickerelweed',
            'ramsons',
            'red_clover']
-
-
-def predict(filename, model):
-    img = load_img(filename, target_size = (224, 224))
-    img = img_to_array(img)/255.
-    img = expand_dims(img, axis = 0)
-    result = model.predict(img)
-
-    dict_result = {}
-    for i in range(35):
-        dict_result[result[0][i]] = classes[i]
-
-    res = result[0]
-    res.sort()
-    res = res[::-1]
-    prob = res[:3]
-
-    prob_result = []
-    class_result = []
-    prob_result.append((prob[0]*100).round(2))
-    class_result.append(dict_result[prob[0]])
-
-    return class_result, prob_result
+app = Flask(__name__)
+model = load_model('model.hdf5', compile=True)
 
 
 @app.route('/')
@@ -86,34 +63,47 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/results', methods=['GET', 'POST'])
-def success():
-    error = ''
-    target_img = os.path.join(os.getcwd(), UPLOAD_FOLDER)
-    if request.method == 'POST':
-        if request.files:
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                img = file.filename
-                class_result, prob_result = predict(img_path, model)
-                predictions = {
-                    "class1": class_result[0],
-                    "prob1": prob_result[0]
-                }
-            else:
-                error = "Please upload an image file"
-            if len(error) == 0:
-                return render_template('results.html', img=img, predictions=predictions)
-            else:
-                return render_template('index.html', error=error)
+@app.route('/api/image', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return render_template('index.html', prediction="Please upload an image.")
+    file = request.files['image']
+
+    if file.filename == '':
+        return render_template('index.html', prediction='Please select an image.')
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        print("***" + filename)
+        ImageFile.LOAD_TRUNCATED_IMAGES = False
+        img = Image.open(BytesIO(file.read()))
+        data = io.BytesIO()
+        img.save(data, "JPEG")
+        encoded_img_data = base64.b64encode(data.getvalue())
+        img.load()
+        img = img.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+        img = img_to_array(img)/255.
+        img = expand_dims(img, axis=0)
+        result = model.predict(img)
+        dict_result = {}
+        for i in range(35):
+            dict_result[result[0][i]] = classes[i]
+
+        res = result[0]
+        res.sort()
+        res = res[::-1]
+        prob = res[:3]
+
+        prob_result = []
+        class_result = []
+        prob_result.append((prob[0] * 100).round(2))
+        class_result.append(dict_result[prob[0]])
+        return render_template('index.html', img_data=encoded_img_data.decode('utf-8'),
+                               prediction='This is most likely an image of the wild edible plant ' + str(class_result[0]) +
+                                          ' with a confidence of ' + str(prob_result[0]) + '%')
     else:
-        return render_template('index.html')
+        return render_template('index.html', prediction='Invalid file extension.')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-#testing
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
